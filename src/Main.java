fc069944b25274506c65350214c5ce1eeaa149ab
Sergei Lychagin <lychagin0@gmail.com>
2021-03-12 14:44:57 +0400

    форматирование

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BmiService service = new BmiService();
         double index = service.calculate(76.50, 1.80);
-        System.out.printf("%.2f\n",index);
+        System.out.printf("%.2f\n", index);
     }
 }
